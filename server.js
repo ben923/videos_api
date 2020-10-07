901ca024a0c23ade7e543d36d917ebdcd2c50e38
benjamin <benjamin.rouge@tightr.com>
2020-10-07 14:29:54 +0200

    export express in a module

--- server.js
+++ server.js
@@ -1,46 +1,25 @@
-const express = require('express');
-const bootstrap = require('./bootstrap');
 const App = require('./app');
 const init = require('./init');
-const config = require('config');
+const initExpress = require('./express');
 
-let port = 1337;
-
-if (config.has('server.port')) {
-    port = config.get('server.port');
-}
-
-const server = express();
-
-for (const service in bootstrap) {
-    const serviceExecutor = bootstrap[service];
-
-    server.use(serviceExecutor);
-
-    console.log(`loaded bootstrap script ${service}`);
-}
 
 console.log('initing modules');
 
 init()
     .then(modules => {
         console.log('charging app');
-        new App({
-            modules,
-            server
-        });
-
-        server.get('/', (req, res) => {
-            res.status(200).json({
-                api_status: "OK"
-            }).end()
+        initExpress()
+        .then(server => {
+            new App({
+                modules,
+                server
+            });
+        })
+        .catch(err => {
+            console.log(err);
         })
     })
     .catch(err => {
         console.log(err);
 
     })
-
-server.listen(port, () => {
-    console.log(`listening on port ${port}`);
-});
