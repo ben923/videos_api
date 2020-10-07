c4be1dd155a69fd0fc5cc99c4cccebd45e816ddc
benjamin <benjamin.rouge@tightr.com>
2020-10-07 14:30:29 +0200

    es exported in a module

--- app/database/hooks/video.js
+++ app/database/hooks/video.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const es = require('../elasticsearch');
+const init = require('../elasticsearch');
 
 const toSearchableObject = (video) => {
     return new Promise((resolve, reject) => {
@@ -22,76 +22,80 @@ const toSearchableObject = (video) => {
 
 const esIndex = (video) => {
     toSearchableObject(video).then(searchableVideo => {
+        init().then(es => {
 
-        console.log('indexing video')
-    
-        es.search({
-            index: 'videos',
-            size: 1,
-            body: {
-                query: {
-                    match: {
-                        id: searchableVideo.id
+            console.log('indexing video')
+        
+            es.search({
+                index: 'videos',
+                size: 1,
+                body: {
+                    query: {
+                        match: {
+                            id: searchableVideo.id
+                        }
                     }
                 }
-            }
-        }, (err, result) => {
-            if(err){
-                console.log(err);
-            }
-            const hits = result.body.hits.hits;
-    
-            if(!hits[0]){
-                es.index({
-                    index: 'videos',
-                    body: searchableVideo
-                }, (err, res) => {
-                    if(!err){
-                        console.log('successfully indexed video');
-                    } else {
-                        console.log(err);
-                    }
-                });
-            } else {
-                const video = hits[0];
-                es.index({
-                    index: 'videos',
-                    id: video._id,
-                    body: searchableVideo
-                }, (err, res) => {
-                    if(!err){
-                        console.log('successfully indexed video');
-                    } else {
-                        console.log(err);
-                    }
-                });
-            }
+            }, (err, result) => {
+                if(err){
+                    console.log(err);
+                }
+                const hits = result.body.hits.hits;
+        
+                if(!hits[0]){
+                    es.index({
+                        index: 'videos',
+                        body: searchableVideo
+                    }, (err, res) => {
+                        if(!err){
+                            console.log('successfully indexed video');
+                        } else {
+                            console.log(err);
+                        }
+                    });
+                } else {
+                    const video = hits[0];
+                    es.index({
+                        index: 'videos',
+                        id: video._id,
+                        body: searchableVideo
+                    }, (err, res) => {
+                        if(!err){
+                            console.log('successfully indexed video');
+                        } else {
+                            console.log(err);
+                        }
+                    });
+                }
+            })
         })
     });
 }
 
 const esDelete = (video) => {
-    es.search({
-        size: 1,
-        body: {
-            query: {
-                match: {
-                    id: video.id
+    init().then(es => {
+        es.search({
+            size: 1,
+            body: {
+                query: {
+                    match: {
+                        id: video.id
+                    }
                 }
             }
-        }
-    }, (err, res) => {
-        if(!err){
-            const hits = res.body.hits.hits;
-
-            if(hits[0]){
-                es.delete({
-                    index: 'videos',
-                    id: hits[0]._id
-                });
+        }, (err, res) => {
+            if(!err){
+                const hits = res.body.hits.hits;
+    
+                if(hits[0]){
+                    es.delete({
+                        index: 'videos',
+                        id: hits[0]._id
+                    });
+                }
+    
             }
-
-        }
+        })
     })
 }
 
