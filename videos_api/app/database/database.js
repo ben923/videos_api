f038abc3fe1ae9336c38bd2436ba5236beeca76a
Ben923 <benjamin.rouge@epitech.eu>
2020-10-17 19:06:56 +0200

    promises usage

--- videos_api/app/database/database.js
+++ videos_api/app/database/database.js
@@ -3,21 +3,16 @@ const initModels = require('./models/initModels');
 const { Sequelize } = require('sequelize');
 
 const init = () => {
-    return new Promise((resolve, reject) => {
-        const userPath = 'app.database.user', hostPath = 'app.database.host', passPath = 'app.database.pass';
-        if (config.has(passPath) && config.has(hostPath) && config.has(userPath)) {
-            const user = config.get(userPath), host = config.get(hostPath), pass = config.get(passPath);
-            const sequelize = new Sequelize(`postgres://${user}:${pass}@${host}:5432/videos`, { logging: false });
+    const userPath = 'app.database.user', hostPath = 'app.database.host', passPath = 'app.database.pass';
+    if (config.has(passPath) && config.has(hostPath) && config.has(userPath)) {
+        const user = config.get(userPath), host = config.get(hostPath), pass = config.get(passPath);
+        const sequelize = new Sequelize(`postgres://${user}:${pass}@${host}:5432/videos`, { logging: false });
 
-            sequelize.authenticate()
-                .then(() => {
-                    initModels(sequelize)
-                    .then(models => resolve(models))
-                    .catch(err => reject(err))
-                })
-                .catch(err => reject(err))
-        }
-    })
+        return sequelize.authenticate()
+            .then(() => initModels(sequelize))
+            .then(models => resolve(models))
+            .catch(err => reject(err))
+    }
 }
 
 module.exports = init;
