d688604a83fd56e4b7420c77f2dead66e81501bc
ben923 <benjamin.rouge@epitech.eu>
2020-10-17 15:35:13 +0200

    fix elasticsearch container connection refused

--- videos_api/app/database/elasticsearch.js
+++ videos_api/app/database/elasticsearch.js
@@ -2,11 +2,17 @@ const {Client} = require('@elastic/elasticsearch');
 const config = require('config');
 
 const init = () => {
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
         const hostPath = 'app.elastic.host'
         if(config.has(hostPath)){
             const host = config.get(hostPath);
             var client = new Client({node: `http://${host}`});
+
+            await client.indices.create({
+                index: 'videos'
+            })
+            .catch(err => console.log(err));
+
             resolve(client)
         } else {
             reject(Error('missing configuration'));
