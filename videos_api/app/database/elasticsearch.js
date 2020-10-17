fcbc07816dfb4ac1e36b0b2dc18a7866f7aff36c
ben923 <benjamin.rouge@epitech.eu>
2020-10-17 19:58:46 +0200

    promise usage

--- videos_api/app/database/elasticsearch.js
+++ videos_api/app/database/elasticsearch.js
@@ -1,26 +1,23 @@
-const {Client} = require('@elastic/elasticsearch');
+const { Client } = require('@elastic/elasticsearch');
 const config = require('config');
 
 const init = () => {
-    return new Promise(async (resolve, reject) => {
-        const hostPath = 'app.elastic.host'
-        if(config.has(hostPath)){
-            const host = config.get(hostPath);
-            var client = new Client({node: `http://${host}`});
+    const hostPath = 'app.elastic.host'
+    if (config.has(hostPath)) {
+        const host = config.get(hostPath);
+        var client = new Client({ node: `http://${host}` });
 
-            await client.indices.create({
-                index: 'videos'
-            })
+        return client.indices.create({
+            index: 'videos'
+        })
             .catch(err => {
-                if(err.message != 'resource_already_exists_exception')
+                if (err.message != 'resource_already_exists_exception')
                     return reject(console.log(err.message))
-            });
-
-            return resolve(client)
-        } else {
-            return reject(Error('missing configuration'));
-        }
-    })
+            })
+            .then(() => resolve(client));
+    } else {
+        return Promise.reject(Error('missing configuration'));
+    }
 }
 
 module.exports = init
