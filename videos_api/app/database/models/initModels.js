d910527c421b42493bc4af94fed0a39333c84162
Ben923 <benjamin.rouge@epitech.eu>
2020-10-17 17:54:54 +0200

    disable hooks

--- videos_api/app/database/models/initModels.js
+++ videos_api/app/database/models/initModels.js
@@ -21,11 +21,11 @@ const init = (sequelize) => {
         }
     }, {
         timestamps: true,
-        hooks: {
+        // hooks: {
             // afterCreate,
             // afterUpdate,
             // afterDestroy,
-        }
+        // }
     });
 
     const Tag = sequelize.define('Tag', {
