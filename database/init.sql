9a5df3b6402e4612a96516aaba9907a130ef4ec4
ben923 <benjamin.rouge@epitech.eu>
2020-10-18 21:30:33 +0200

    fix error on database creation

--- database/init.sql
+++ database/init.sql
@@ -1,4 +1,13 @@
-SELECT 'CREATE DATABASE videos'
-WHERE NOT EXISTS (SELECT FROM videos WHERE datname = 'videos')\gexec;
+DO
+$do$
+BEGIN
+   IF EXISTS (SELECT FROM pg_database WHERE datname = 'videos') THEN
+      RAISE NOTICE 'Database already exists';  -- optional
+   ELSE
+      PERFORM dblink_exec('dbname=' || current_database()  -- current db
+                        , 'CREATE DATABASE videos');
+   END IF;
+END
+$do$;
 CREATE USER benjamin WITH PASSWORD 'root1337489417';
 GRANT ALL PRIVILEGES ON DATABASE "videos" to benjamin;
